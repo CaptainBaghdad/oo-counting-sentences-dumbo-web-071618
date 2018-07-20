90bd912e4387bc96a31d433f44fa8a465f2768da
Jason Pitts <juniorjr083@gmail.com>
2018-07-20 16:27:11 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".") 
     
   end
 
   def question?
-
+    self.end_with?("?") 
   end
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
-
+   # self.strip.split(/\w[?!.]/).length > 0 ? self.strip.split(/\w[?!.]/).length : 0
+    #puts self.strip.split(/\w[?!.]/)
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    #
   end
 end
